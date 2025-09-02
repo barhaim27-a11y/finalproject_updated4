a11001de114a0df0cfbafe0a1072b5549a11019a
barhaim27-a11y <bar.haim27@gmail.com>
2025-09-02 23:39:58 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -289,7 +289,6 @@ with tab_dash:
                                         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
                                         trained_models = {}
                                         metrics_comp = {}
-        
     for m in chosen_models:
         if m == "LogisticRegression":
             model = Pipeline([
